a313b45673198368a8bd9450ec5e9ad4d9be7596
Nehal Udyavar <nehaludyavar@gmail.com>
2017-07-30 16:14:09 -0700

    Added sankey diagram JSON data

--- js/fuel-flow-chart.js
+++ js/fuel-flow-chart.js
@@ -1,14 +1,15 @@
 
 var margin = 100,
     width = 960 - margin,
-    height = 550 - margin;
+    flow_height = 500 - margin; /* for some reason this gets confused with energy-chart's height
+                                   (if named 'height') */
 
 var svg = d3.select('#fuel-flow-chart')
             .append('div')
             .classed('svg-container', true)
             .append('svg')
             .attr('preserveAspectRatio', 'xMinyMin meet')
-            .attr('viewBox', '0 0 ' + (width + margin) + ' ' + (height + margin))
+            .attr('viewBox', '0 0 ' + (width + margin) + ' ' + (flow_height + margin))
             .classed('svg-content-responsive', true)
 
 var sankey = d3.sankey();
