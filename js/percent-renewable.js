bbe0cc2856db5b49701e428506139cddd38eab1c
Nehal Udyavar <nehaludyavar@gmail.com>
2018-01-27 16:20:08 -0800

    Set up axes for percent-renewable-chart

--- js/percent-renewable.js
+++ js/percent-renewable.js
@@ -10,14 +10,88 @@ d3.csv("data/percentage-renewables.csv", type, function(error, data) {
   if (error) throw error;
 
   // Setting margin, width, height of plot
-  var margin = 130,
+  var margin = 100,
       width = 850 - margin,
       height = 550 - margin;
 
-  var svg = d3.select('percent-renewable-chart')
+  var svg = d3.select('#percent-renewable-chart')
               .append('svg')
               .attr('viewBox', '0 0 ' + (width + margin) + ' ' + (height + margin));
 
   var g = svg.append('g')
              .attr('transform', 'translate(' + 50 + ',' + 20 + ')');
+
+  var x = d3.scaleTime()
+            .rangeRound([0, width]);
+
+  var y = d3.scaleLinear()
+            .rangeRound([height, 0]);
+
+  var z = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Initialise d3 line function
+  var line = d3.line()
+               .defined(function(d) { return !isNaN(d['Energy']); })
+               .curve(d3.curveLinear)
+               .x(function(d) {
+                 return x(d['Year']);
+               })
+               .y(function(d) {
+                 return y(d['Percentage']);
+               });
+
+  var countries = data.columns.slice(1).map(function(id) {
+    return {
+      id: id,
+      values: data.map(function(d) {
+        return {Year: d['Year'], Percentage: d[id]};
+      })
+    };
+  });
+
+  // Set x- and y-axis domains
+  x.domain(d3.extent(data, function(d) { return d['Year']; }));
+
+  y.domain([0, 100]);
+
+  color.domain(countries.map(function(c) {
+    return c.id;
+  }));
+
+  // Set x- and y-axes
+  var x_axis = d3.axisBottom(x);
+
+  var y_axis = d3.axisLeft(y);
+
+  x_axis.ticks(6)
+        .tickSizeOuter(0)
+        .tickSizeInner(5)
+        .tickPadding(4);
+
+  y_axis.ticks(5)
+        .tickSizeInner(-width)
+        .tickSizeOuter(0)
+        .tickPadding(8);
+
+  // Add x-axis to plot
+  g.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .attr('id', 'percent-renewable-x-axis')
+    .attr('class', 'line-chart-x-axis')
+    .call(x_axis);
+
+  // Add y-axis to plot
+  g.append('g')
+    .attr('id', 'percent-renewable-y-axis')
+    .attr('class', 'line-chart-y-axis')
+    .call(y_axis);
+
 })
+
+function type(d, _, columns) {
+  d['Year'] = parseYear(d['Year']);
+  for (var i = 1, n = columns.length, c; i < n; ++i) {
+    d[c = columns[i]] = +d[c];
+    return d;
+  }
+}
