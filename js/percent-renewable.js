e5466e25326765720e0929913e5ef38dbdb66bbd
Nehal Udyavar <nehaludyavar@gmail.com>
2018-03-28 00:48:09 -0700

    Labels for bar chart showing percentage (not sure about black + white)

--- js/percent-renewable.js
+++ js/percent-renewable.js
@@ -27,11 +27,30 @@ d3.json('data/percent-renewable.json', function(error, data) {
              .attr('class', 'country-name')
              .text(country_name);
 
-    table_row.append('td')
-             .attr('class', 'country-percent-renewable')
-             .append('div')
-              .classed('percent-bar', true)
-              .style('width', barScale(percent_renewable) + 'px');
+    var bar = table_row.append('td')
+                .attr('class', 'country-percent-renewable')
+                .append('div')
+                 .classed('percent-bar', true)
+                 .style('width', barScale(percent_renewable) + 'px')
+
+    if (percent_renewable <= 1) {
+      bar.append('text')
+         .attr('class', 'percent-renewable-label')
+          .text(percent_renewable.toFixed(1) + '%');
+    } else {
+      if (Math.floor(percent_renewable) <= 4) {
+        bar.append('text')
+           .attr('class', 'percent-renewable-label')
+            .text(Math.floor(percent_renewable) + '%');
+      } else {
+        bar.append('text')
+           .attr('class', 'percent-renewable-label')
+           .style('color', 'white')
+            .text(Math.floor(percent_renewable) + '%');
+      }
+
+    }
+
   };
 
 
