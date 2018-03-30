b9e60b91dd63e7ae40a0a8e73a79393034cfadc2
Nehal Udyavar <nehaludyavar@gmail.com>
2018-03-29 21:02:53 -0700

    Commented script + refactored table populating code

--- js/percent-renewable.js
+++ js/percent-renewable.js
@@ -1,4 +1,4 @@
-
+// All regions for countries in percent-renewable dataset
 const REGIONS = ['East Asia & Pacific', 'Europe & Central Asia',
                  'Latin America & Caribbean', 'Middle East & North Africa',
                  'North America', 'South Asia', 'Sub-Saharan Africa'];
@@ -10,11 +10,12 @@ d3.json('data/percent-renewable.json', function(error, data) {
 
   region_controls.append('a')
                  .classed('region-button', true)
-                 .classed('first-button-curve', true)
+                 .classed('first-button-curve', true)   // Rounded rectangle
+                 .style('width', 100/(REGIONS.length + 1) + '%') // Divide widths for each region equally
                  .text('All Regions')
                  .on('click', function(o) {
                    var region = d3.select(this).text();
-
+                   // Call sorting function
                    sortByRegion(ordered_data, region);
                  });;
 
@@ -26,27 +27,50 @@ d3.json('data/percent-renewable.json', function(error, data) {
                                        .text(REGIONS[i])
                                        .on('click', function(o) {
                                          var region = d3.select(this).text();
-
+                                         // Call sorting function
                                          sortByRegion(ordered_data, region);
                                        });
 
+    // If it's the last button, add the rounded rectangle curves; otherwise, normal buttons.
     if (i == REGIONS.length - 1) {
       region_button.classed('last-button-curve', true);
     } else {
       region_button;
     }
   }
+
+  // Get width of the table
   var chartWidth = d3.select('#percent-renewable-table').node().offsetWidth,
-      barWidth = Math.floor(chartWidth * .5);
+      barWidth = Math.floor(chartWidth * .5); // CSS: Percent Renewable column is set as 50%.
       //barHeight = d3.select('#percent-renewable-column').node().offsetHeight * 0.9;
 
+  // Create a scale for the bar charts.
   var barScale = d3.scaleLinear()
                    .domain([0, 100])
                    .range([1, barWidth]);
 
+  // Rearrange data in descending order of percentage.
   var ordered_data = orderData(data);
 
+  // Add table rows for each country, adding the name, region, and drawing the bar graph.
   for (i = 0; i < ordered_data.length; i++) {
+    populateTable(ordered_data);
+  };
+
+  // Rearrange data in descending order of percentage.
+  function orderData(data) {
+    for (i = 0; i < data.length; i++) {
+      data.sort(function(x, y) {
+        return d3.descending(x.percent, y.percent);
+      })
+    }
+
+    return data;
+  }
+
+  // Add table rows for each country, adding the name, region, and drawing the bar graph.
+  // Also, add formatting for percentage number labels.
+  function populateTable(ordered_data) {
     var country_info = ordered_data[i],
         country_name = country_info.name,
         region = country_info.region,
@@ -71,40 +95,34 @@ d3.json('data/percent-renewable.json', function(error, data) {
 
     // There is probably a much better way to do this but it's late so I'm going to commit
     if (percent_renewable <= 1 && percent_renewable > 0) {
+      // Make sure doesn't round down to zero (show two decimal places)
       if (percent_renewable < 0.05) {
         bar.append('text')
            .attr('class', 'percent-renewable-label')
             .text(percent_renewable + '%');
       } else {
+        // Show to only 1 decimal place
         bar.append('text')
            .attr('class', 'percent-renewable-label')
             .text(percent_renewable.toFixed(1) + '%');
       }
     } else {
       if (Math.floor(percent_renewable) <= 4) {
+        // Show in black colour because text is over white background.
         bar.append('text')
            .attr('class', 'percent-renewable-label')
             .text(Math.floor(percent_renewable) + '%');
       } else {
+        // Show in white colour because text is over blue bar chart.
         bar.append('text')
            .attr('class', 'percent-renewable-label')
            .style('color', 'white')
             .text(Math.floor(percent_renewable) + '%');
       }
     }
-
-  };
-
-  function orderData(data) {
-    for (i = 0; i < data.length; i++) {
-      data.sort(function(x, y) {
-        return d3.descending(x.percent, y.percent);
-      })
-    }
-
-    return data;
   }
 
+  // Sorts data to show only data for selected regions.
   function sortByRegion(ordered_data, region) {
     // Remove all existing rows in table
     d3.selectAll('#percent-renewable-table tbody tr').remove();
@@ -112,100 +130,12 @@ d3.json('data/percent-renewable.json', function(error, data) {
     if (region != 'All Regions') {
       for (i = 0; i < ordered_data.length; i++) {
         if (region == ordered_data[i].region) {
-          var country_info = ordered_data[i],
-              country_name = country_info.name,
-              region = country_info.region,
-              percent_renewable = country_info.percent;
-
-          var table_row = d3.select('#percent-renewable-table tbody')
-                            .append('tr');
-
-          table_row.append('td')
-                   .attr('class', 'rank')
-                   .text('');
-
-          table_row.append('td')
-                   .attr('class', 'country-name')
-                   .text(country_name);
-
-          var bar = table_row.append('td')
-                      .attr('class', 'country-percent-renewable')
-                      .append('div')
-                       .classed('percent-bar', true)
-                       .style('width', barScale(percent_renewable) + 'px')
-
-          // There is probably a much better way to do this but it's late so I'm going to commit
-          if (percent_renewable <= 1 && percent_renewable > 0) {
-            if (percent_renewable < 0.05) {
-              bar.append('text')
-                 .attr('class', 'percent-renewable-label')
-                  .text(percent_renewable + '%');
-            } else {
-              bar.append('text')
-                 .attr('class', 'percent-renewable-label')
-                  .text(percent_renewable.toFixed(1) + '%');
-            }
-          } else {
-            if (Math.floor(percent_renewable) <= 4) {
-              bar.append('text')
-                 .attr('class', 'percent-renewable-label')
-                  .text(Math.floor(percent_renewable) + '%');
-            } else {
-              bar.append('text')
-                 .attr('class', 'percent-renewable-label')
-                 .style('color', 'white')
-                  .text(Math.floor(percent_renewable) + '%');
-            }
-          }
+          populateTable(ordered_data);
         }
       }
     } else {
       for (i = 0; i < ordered_data.length; i++) {
-        var country_info = ordered_data[i],
-            country_name = country_info.name,
-            region = country_info.region,
-            percent_renewable = country_info.percent;
-
-        var table_row = d3.select('#percent-renewable-table tbody')
-                          .append('tr');
-
-        table_row.append('td')
-                 .attr('class', 'rank')
-                 .text('');
-
-        table_row.append('td')
-                 .attr('class', 'country-name')
-                 .text(country_name);
-
-        var bar = table_row.append('td')
-                    .attr('class', 'country-percent-renewable')
-                    .append('div')
-                     .classed('percent-bar', true)
-                     .style('width', barScale(percent_renewable) + 'px')
-
-        // There is probably a much better way to do this but it's late so I'm going to commit
-        if (percent_renewable <= 1 && percent_renewable > 0) {
-          if (percent_renewable < 0.05) {
-            bar.append('text')
-               .attr('class', 'percent-renewable-label')
-                .text(percent_renewable + '%');
-          } else {
-            bar.append('text')
-               .attr('class', 'percent-renewable-label')
-                .text(percent_renewable.toFixed(1) + '%');
-          }
-        } else {
-          if (Math.floor(percent_renewable) <= 4) {
-            bar.append('text')
-               .attr('class', 'percent-renewable-label')
-                .text(Math.floor(percent_renewable) + '%');
-          } else {
-            bar.append('text')
-               .attr('class', 'percent-renewable-label')
-               .style('color', 'white')
-                .text(Math.floor(percent_renewable) + '%');
-          }
-        }
+        populateTable(ordered_data);
       }
     }
   }
